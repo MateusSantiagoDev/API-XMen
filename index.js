518b1326d91070fd076f4e11c1e16fab401dc9db
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-09-12 11:58:41 -0300

    add swagger structure

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express, { Router } from "express";
 import cors from "cors";
+
 import { dataBaseConnect } from "./dataBase/mongoDB/connection/connect.js";
 import { makeCreate } from "./factories/createFactory.js";
 import { makeUpdate } from "./factories/updateFactory.js";
 import { makeFindAll } from "./factories/findAllFactory.js";
 import { makeFindById } from "./factories/findByIdFactory.js";
 import { makeDelete } from "./factories/deleteFactory.js";
+
+import swageer from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert{ type: "json" };
 
 
 const DataBase = new dataBaseConnect();
@@ -23,6 +27,9 @@ const delet = makeDelete(router);
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api-docs", swageer.serve);
+app.get("/api-docs", swageer.setup(swaggerDocument));
 
 app.use("/characters", create.route());
 app.use("/characters", update.route());
