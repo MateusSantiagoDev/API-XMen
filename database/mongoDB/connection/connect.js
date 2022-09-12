e7f2e97b64c6a562385e16cd7119197d96eadb4e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-09-12 19:09:03 -0300

    deploy config

--- database/mongoDB/connection/connect.js
+++ database/mongoDB/connection/connect.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+
 export class dataBaseConnect{
     async connection(){
         await mongoose.connect(process.env.DATABASE_URL);
