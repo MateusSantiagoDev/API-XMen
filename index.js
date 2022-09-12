70d5e55ce33fd7e408121eb1ad47843976ad8be9
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-09-12 10:40:38 -0300

    add index structure

---
+++ index.js
@@ -0,0 +1,33 @@
+import express, { Router } from "express";
+import cors from "cors";
+import { dataBaseConnect } from "./dataBase/mongoDB/connection/connect.js";
+import { makeCreate } from "./factories/createFactory.js";
+import { makeUpdate } from "./factories/updateFactory.js";
+import { makeFindAll } from "./factories/findAllFactory.js";
+import { makeFindById } from "./factories/findByIdFactory.js";
+import { makeDelete } from "./factories/deleteFactory.js";
+
+
+const DataBase = new dataBaseConnect();
+await DataBase.connection();
+
+const port = process.env.PORT || 3000;
+const app = express();
+const router = Router();
+
+const create = makeCreate(router);
+const update = makeUpdate(router);
+const findAll = makeFindAll(router);
+const findById = makeFindById(router);
+const delet = makeDelete(router);
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/characters", create.route());
+app.use("/characters", update.route());
+app.use("/characters", findAll.route());
+app.use("/characters", findById.route());
+app.use("/characters", delet.route());
+
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
