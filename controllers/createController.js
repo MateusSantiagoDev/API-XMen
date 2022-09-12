4d711f151f9a39ae30c77814768231a50a627bbc
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-09-12 18:08:11 -0300

    update

--- controllers/createController.js
+++ controllers/createController.js
@@ -5,7 +5,8 @@ export class createController {
   
     async execute(req, res) {
       try {
-        const response = await this.create.execute(req.body);
+        const params = req.body
+        const response = await this.create.execute(params);
         res.status(201).send(response);
       } catch (err) {
         res.status(400).send(message.err);
