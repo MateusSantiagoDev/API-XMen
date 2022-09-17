e2de2994a8b4cc7b71e35d6e3c04dbd485717f33
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-09-17 00:14:31 -0300

    update getById controller

--- controllers/findByIdController.js
+++ controllers/findByIdController.js
@@ -8,7 +8,7 @@ export class findByIdController {
         const response = await this.findById.execute(req.params.id);
         res.status(200).send(response);
       } catch (err) {
-        res.status(400).send(message.err);
+        res.status(400).send(err.message);
       }
     }
   }
